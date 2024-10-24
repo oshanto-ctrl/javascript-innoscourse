38c75cd39b2a20875c0f24b6e71bda5700597764
Rejoan Siddiky <siddikyrejoan@gmail.com>
2024-10-24 20:24:06 +0600

    Learning external js file linking

---
+++ begin.js
@@ -0,0 +1,13 @@
+/*
+    Line by line interpreted.
+*/
+
+// Alert pop up to user.
+alert("As-salamualaikum Visitor.");
+
+// Output
+document.write("Hajime");
+document.write(" Mashite.<br>");
+console.log("I love you 3000"); // Browser's console
+document.write(300);
+ 
