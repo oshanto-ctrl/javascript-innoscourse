75bbec49723c3e50f94e9d90683a46a4ef4060b2
Rejoan Siddiky <siddikyrejoan@gmail.com>
2024-10-28 21:28:56 +0600

    Learned about localstorage

---
+++ localstorage.js
@@ -0,0 +1,9 @@
+// Store a value in local storage
+localStorage.setItem('myKey', 'myValue');
+
+// Retrieve the value from local storage
+const value = localStorage.getItem('myKey');
+
+// Print the value in the console
+console.log(value); // Outputs: myValue
+
